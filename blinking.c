24f8a11a96442c0906af215676f84e899fdd9325
Marcus Roth <marcus.roth@plasmics.com>
2024-05-24 12:47:57 +0200

    good state, needs refining in wait function

--- blinking.c
+++ blinking.c
@@ -2,6 +2,7 @@
 #include <stdint.h>
 #include <unistd.h>
 
+
 #include "blinking.h"
 
 typedef void (*fsm_state)(void);
@@ -9,21 +10,87 @@ typedef struct {
 	fsm_state state;
 } fsm_t;
 
-fsm_t fsm;
+fsm_t fsm;										/*!< current state of state machine */
+
+
+/**
+ * \brief           includes all needed variables
+ */
+typedef struct {
+	uint32_t counter;							/*!< how often the led has blinked before*/
+	uint32_t count_to;							/*!< how often the led has should blink over all*/
+
+    //fsm_t fsm;								/*!< current state of state machine */
+
+} led_variables;
+
+led_variables var;
+
+
 
 /*
 typedef void (*fsm_state)(void);
 fsm_state fsm;
 */
 
-//uint32_t counter = 0;
-int counter = 0;
 
 
+// ----------- Functions -------------------
+// -----------------------------------------
+
+/**
+ * \brief           initializes all variables and code needed
+ */
+void init_function(){
+	var.counter = 0;
+	var.count_to = 5; //find naming convention maybe counter_end but cout_to seems better
+}
+
+/**
+ * \brief           all code needed to turn on a LED
+ * \return          returns 0 if successful, 1 on failure
+ */
+void turn_on_led(){
+	printf("\n LED ON");
+
+	// + code needed to turn on a led
+}
+
+/**
+ * \brief           all code needed to turn off a LED
+ * \return          returns 0 if successful, 1 on failure
+ */
+void turn_off_led(){
+	printf("\n LED OFF");
+
+	// + code needed to turn off a led
+}
+
+
+
+
+// ----------- fsm STATES ------------------
+// -----------------------------------------
+
+/**
+ * \brief           start point of state machine
+ */
+void state_start(void) {
+	init_function();
+
+	fsm.state = state_led_on;
+}
+
+/**
+ * \brief           turns on the led and checks 
+ */
 void state_led_on(void) {
-	printf("\n run: %i", counter);
+	printf("\n\n run: %i", var.counter);	
+	turn_on_led();
 
-	if (counter++ < 5) {
+
+	if (var.counter < (var.count_to - 1)) {
+		var.counter++;
 		fsm.state = state_led_off;
 	}
 	else {
@@ -32,28 +99,33 @@ void state_led_on(void) {
 }
 
 void state_led_off(void) {
-	printf("\n off");
+	turn_off_led();
 
 	fsm.state = state_led_on;
 }
 
 void state_end_program(void) {
+	turn_off_led();
 	printf("\n Ending the program.");
 	exit(0);
 }
 
 
+
+
+// --------------------- MAIN ---------------------------
+
 int main() 
 { 
+	fsm.state = state_start;
 
-	fsm.state = state_led_off;
 
 	while(1) {
-		usleep(500 * 1000);  // usleep takes sleep time in microseconds
-		fsm.state();
+		usleep(500 * 1000);  					// usleep takes sleep time in microseconds
+		fsm.state();							//runs current state of state machine
 	}
 
 
-	return 0;
+	return 0;	//program ran successfully 
 } 
 
