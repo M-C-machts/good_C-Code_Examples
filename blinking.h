8c8407b07c7cad2ab76239741337da5023708660
Marcus Roth <marcus-roth@gmx.at>
2024-06-08 10:35:04 +0200

    added correct handling of .h file

--- blinking.h
+++ blinking.h
@@ -1,7 +1,29 @@
+#ifndef BLINKING_H
+#define BLINKING_H
+
+
+
 #include <stdio.h> 
 #include <stdint.h>
 #include <unistd.h>
 
+
+#ifdef __cplusplus
+extern "C" {
+#endif /* __cplusplus */
+
+
+
+
+void state_start(void);
 void state_led_on(void);
 void state_led_off(void);
 void state_end_program(void);
+
+
+
+#ifdef __cplusplus
+}
+#endif /* __cplusplus */
+
+#endif /* BLINKING_H */
